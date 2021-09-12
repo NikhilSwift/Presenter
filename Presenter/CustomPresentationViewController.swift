70289452ebb6a3a9a79b083db89ca879fd33e5d9
Nikhil_Desai <nikhil.logicloop@gmail.com>
2021-09-12 22:03:10 +0530

    Changes

--- Presenter/CustomPresentationViewController.swift
+++ Presenter/CustomPresentationViewController.swift
@@ -7,61 +7,52 @@
 
 import UIKit
 
-class CustomPresentationViewController: UIPresentationController {
+class CustomPresentationViewController: UIViewController {
+    var SetPointOrigin = false
+    var pointOrigin: CGPoint?
+    var mainView: UIView?
+    var minDragVelocity : CGFloat = 1250
+    init (with View: UIView?){
+        self.mainView = View
+        super.init(nibName: nil, bundle: nil)
+    }
     
-    let blurEffectView: UIVisualEffectView!
-    var tapGesturerecognizer = UITapGestureRecognizer()
-    var presentHeight = CGFloat()
-    var presentType : ShapeManager!
-    init(presentedViewController: UIViewController, presenting presentingViewController: UIViewController?,blurEffectStyle:UIBlurEffect.Style,height: CGFloat,type: ShapeManager) {
-        let blurEffect = UIBlurEffect(style: blurEffectStyle)
-        blurEffectView = UIVisualEffectView(effect: blurEffect)
-        self.presentHeight = height
-        self.presentType = type
-        super.init(presentedViewController: presentedViewController, presenting: presentingViewController)
-        tapGesturerecognizer = UITapGestureRecognizer(target: self, action: #selector(dismissController))
-        blurEffectView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        self.blurEffectView.isUserInteractionEnabled = true
-        self.blurEffectView.addGestureRecognizer(tapGesturerecognizer)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .white
+        guard let PresntView = mainView else {return}
+        self.view = PresntView
+        let pangesture = UIPanGestureRecognizer(target: self, action: #selector(panGestureRecognizerAction(sender:)))
+        self.view.addGestureRecognizer(pangesture)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        if !SetPointOrigin{
+            SetPointOrigin = true
+            pointOrigin = self.view.frame.origin
+        }
+    }
+    
+    @objc func panGestureRecognizerAction(sender: UIPanGestureRecognizer) {
+        let translation = sender.translation(in: view)
+        
+        guard translation.y >= 0 else { return }
+        
+        view.frame.origin = .init(x: 0, y: self.pointOrigin!.y + translation.y)
+        if sender.state == .ended {
+            let dragVelocity = sender.velocity(in: view)
+            if dragVelocity.y >= minDragVelocity {
+                self.dismiss(animated: true, completion: nil)
+            } else {
+                UIView.animate(withDuration: 0.2) {
+                    self.view.frame.origin = self.pointOrigin ?? CGPoint(x: 0, y: UIScreen.main.bounds.height - self.view.frame.height)
+                }
+            }
+        }
         
     }
     
-    override var frameOfPresentedViewInContainerView: CGRect{
-        return .init(x: 0, y: self.containerView!.frame.height - presentHeight, width: self.containerView!.frame.width, height: presentHeight)
-    }
-    
-    
-    override func presentationTransitionWillBegin() {
-        self.blurEffectView.alpha = 0
-        self.containerView?.addSubview(blurEffectView)
-        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) in
-            self.blurEffectView.alpha = 1.0
-        }, completion: { (UIViewControllerTransitionCoordinatorContext) in })
-    }
-    
-    override func dismissalTransitionWillBegin() {
-        self.presentedViewController.transitionCoordinator?.animate(alongsideTransition: { (UIViewControllerTransitionCoordinatorContext) in
-            self.blurEffectView.alpha = 0
-        }, completion: { (UIViewControllerTransitionCoordinatorContext) in
-            self.blurEffectView.removeFromSuperview()
-        })
-    }
-    
-    override func containerViewWillLayoutSubviews() {
-        super.containerViewWillLayoutSubviews()
-        presentedView?.setShape(presentType)
-    }
-    
-    override func containerViewDidLayoutSubviews() {
-        super.containerViewDidLayoutSubviews()
-        presentedView?.frame = frameOfPresentedViewInContainerView
-        blurEffectView.frame = containerView!.bounds
-    }
-    
-   @objc func dismissController(){
-        self.presentedViewController.dismiss(animated: true, completion: nil)
-    }
 }
-
-
-
